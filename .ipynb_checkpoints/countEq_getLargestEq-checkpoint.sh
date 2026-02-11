29f3089766fdfd9582fe727659e19f0e1048c272
Your Name <you@example.com>
2026-02-10 21:18:32 -0500

    fixed number 10

--- .ipynb_checkpoints/countEq_getLargestEq-checkpoint.sh
+++ .ipynb_checkpoints/countEq_getLargestEq-checkpoint.sh
@@ -3,5 +3,11 @@
 y=$1
 file=Year_Mag_Country.tsv
 
+echo "Total Earthquakes."
+
 grep "${y}" "$file"| wc -l 
+
+echo "Largest Earthquake Mag for a Given Country."
+
 grep "${y}" "$file"| sort -k 2 -n |tail -1
+
